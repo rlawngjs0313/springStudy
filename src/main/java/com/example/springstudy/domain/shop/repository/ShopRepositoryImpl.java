9b6ad03098aa2fff8528bd00856541a7eb75ad4e
Juheon <juheon0313@naver.com>
2025-04-18 00:28:51 +0900

    🔨[Refactor] 안쓰는 필드 정리

--- src/main/java/com/example/springstudy/domain/shop/repository/ShopRepositoryImpl.java
+++ src/main/java/com/example/springstudy/domain/shop/repository/ShopRepositoryImpl.java
@@ -1,13 +1,9 @@
 package com.example.springstudy.domain.shop.repository;
 
-import com.example.springstudy.domain.shop.entity.QShop;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class ShopRepositoryImpl implements ShopRepositoryCustom {
-    private final JPAQueryFactory queryFactory;
-    private final QShop shop = QShop.shop;
 }
