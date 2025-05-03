56f099a1a5e94dac6d736d37a430262faf47e9ed
Juheon <juheon0313@naver.com>
2025-05-03 16:03:02 +0900

    ✨ [feat] ReasonDTO 설계

---
+++ src/main/java/com/example/springstudy/global/apiPayload/dto/ReasonDTO.java
@@ -0,0 +1,17 @@
+package com.example.springstudy.global.apiPayload.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class ReasonDTO {
+
+    private HttpStatus status;
+
+    private final boolean isSuccess;
+    private final String code;
+    private final String message;
+}
