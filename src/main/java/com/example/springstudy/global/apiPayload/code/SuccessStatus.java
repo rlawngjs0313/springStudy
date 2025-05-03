0ed8a297de00f2dfdc6ec02760fae14a2f632884
Juheon <juheon0313@naver.com>
2025-05-03 16:01:52 +0900

    ✨ [feat] 성공, 실패 상태 코드 정의

---
+++ src/main/java/com/example/springstudy/global/apiPayload/code/SuccessStatus.java
@@ -0,0 +1,36 @@
+package com.example.springstudy.global.apiPayload.code;
+
+import com.example.springstudy.global.apiPayload.dto.ReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    OK(HttpStatus.OK, "COMMON200", "성공적으로 처리했습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDTO getReason() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .build();
+    }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .status(status)
+                .build();
+    }
+}
