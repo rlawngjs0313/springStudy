6dab1ce8eceb514a5505ba4a7dbc47bee8aee741
Juheon <juheon0313@naver.com>
2025-05-07 17:14:44 +0900

    ✨ [feat] User Repository 생성, Service 생성 (회원가입, 로그인 구현)

---
+++ src/main/java/com/example/springstudy/domain/user/service/query/UserQueryService.java
@@ -0,0 +1,4 @@
+package com.example.springstudy.domain.user.service.query;
+
+public interface UserQueryService {
+}
---
+++ src/main/java/com/example/springstudy/domain/user/service/query/UserQueryServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.springstudy.domain.user.service.query;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueryServiceImpl implements UserQueryService {
+
+}
