63f6d7c7a825572a1408fa601321b5615170fa89
Juheon <juheon0313@naver.com>
2025-05-07 17:19:52 +0900

    ✨ [feat] CustomUserDetails, Service 구현

---
+++ src/main/java/com/example/springstudy/global/auth/service/query/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.springstudy.global.auth.service.query;
+
+import com.example.springstudy.domain.user.entity.User;
+import com.example.springstudy.domain.user.exception.UserException;
+import com.example.springstudy.domain.user.exception.code.UserErrorCode;
+import com.example.springstudy.domain.user.repository.UserRepository;
+import com.example.springstudy.global.auth.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String email){
+        User user = userRepository.findUserByEmail(email).orElseThrow(() ->
+                new UserException(UserErrorCode.NOT_FOUND));
+        return new CustomUserDetails(user);
+    }
+}
