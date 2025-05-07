244f552dc9618e0704c277a7aef767a91f43a594
Juheon <juheon0313@naver.com>
2025-05-07 17:21:23 +0900

    ✨ [feat] JWT 로그인 구현 2/2 (JWT 필터 구현, 예외 처리, 응답 통일)

---
+++ src/main/java/com/example/springstudy/global/auth/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,40 @@
+package com.example.springstudy.global.auth.exception;
+
+import com.example.springstudy.domain.user.exception.code.UserErrorCode;
+import com.example.springstudy.global.apiPayload.ApiResponse;
+import com.example.springstudy.global.apiPayload.code.BaseErrorCode;
+import com.example.springstudy.global.apiPayload.code.ErrorStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException, ServletException {
+
+        UserErrorCode code = UserErrorCode.FORBIDDEN;
+        response.setContentType("application/json; charset=UTF-8");
+        response.setStatus(code.getStatus().value());
+
+        ApiResponse<Object> errorResponse = ApiResponse.onFailure(
+                code.getCode(),
+                code.getMessage(),
+                null
+        );
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), errorResponse);
+    }
+}
